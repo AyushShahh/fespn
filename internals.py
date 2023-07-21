72dd062af6711ae0f4e13a5a5f4c7dc2b75fcc61
Ayush Shah <shahayush001@gmail.com>
2023-07-21 20:14:54 +0530

    refactor and remove code repetition

--- internals.py
+++ internals.py
@@ -1,6 +1,13 @@
 from csv import DictReader, DictWriter
 from math import ceil
 
+def score(m1, m2):
+    i = (m1 + m2) / 4
+    if i >= 12:
+        i += 4
+        if i > 30:
+            i= 30
+    return ceil(i)
 
 # Calculate the required fields and write to the output CSV
 with open('students_data.csv', 'r') as csvfile_in, open('internals.csv', 'w', newline='') as csvfile_out:
@@ -10,47 +17,15 @@ with open('students_data.csv', 'r') as csvfile_in, open('internals.csv', 'w', ne
     writer.writeheader()
 
     for row in reader:
-
-        maths_internal = (int(row['mat2mse1']) + int(row['mat2mse2'])) / 4
-        if maths_internal >= 12:
-            maths_internal += 4
-            if maths_internal > 30:
-                maths_internal = 30
-        
-
-        bee_internal = (int(row['beemse1']) + int(row['beemse2'])) / 4
-        if bee_internal >= 12:
-            bee_internal += 4
-            if bee_internal > 30:
-                bee_internal = 30
-        
-        egd_internal = (int(row['egdmse1']) + int(row['egdmse2'])) / 4
-        if egd_internal >= 12:
-            egd_internal += 4
-            if egd_internal > 30:
-                egd_internal = 30
-        
-        pps_internal = (int(row['ppsmse1']) + int(row['ppsmse2'])) / 4
-        if pps_internal >= 12:
-            pps_internal += 4
-            if pps_internal > 30:
-                pps_internal = 30
-
-        eng_internal = (int(row['engmse1']) + int(row['engmse2'])) / 4
-        if eng_internal >= 12:
-            eng_internal += 4
-            if eng_internal > 30:
-                eng_internal = 30
-
         full = 16 if row['full'] == '1' else 12
         bonus_marks = (float(row['attendance']) * full) / 100
 
         writer.writerow({
             'name': row['name'],
-            'mat_i': ceil(maths_internal),
-            'bee_i': ceil(bee_internal),
-            'egd_i': ceil(egd_internal),
-            'pps_i': ceil(pps_internal),
-            'eng_i': ceil(eng_internal), 
+            'mat_i': score(int(row['mat2mse1']), int(row['mat2mse2'])),
+            'bee_i': score(int(row['beemse1']), int(row['beemse2'])),
+            'egd_i': score(int(row['egdmse1']), int(row['egdmse2'])),
+            'pps_i': score(int(row['ppsmse1']), int(row['ppsmse2'])),
+            'eng_i': score(int(row['engmse1']), int(row['engmse2'])), 
             'bonus': ceil(bonus_marks)
         })
